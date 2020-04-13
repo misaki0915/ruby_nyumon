7e14be4fa570b1cdeb24ed4363b58ca4910bfe93
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-13 20:35:12 +0900

    p.78 FizzBuzzプログラムのテスト自動化

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -10,14 +10,16 @@ def fizz_buzz(sum)
   end
 end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(11)
-puts fizz_buzz(12)
-puts fizz_buzz(13)
-puts fizz_buzz(14)
-puts fizz_buzz(15)
-puts fizz_buzz(16)
+require 'minitest/autorun'
+
+class FizzBuzztest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+  end
+end
