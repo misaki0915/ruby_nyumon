a09c9e176748c24fb4e60e8860be405f59510926
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-13 17:46:23 +0900

    p.38 FizzBuzzプログラムを修正

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,17 +1,23 @@
 def fizz_buzz(sum)
-  answer = ''
-  if sum % 3 == 0
-    answer += 'Fizz'
+  if sum % 15 == 0
+    'Fizz Buzz'
+  elsif sum % 3 == 0
+    'Fizz'
+  elsif sum % 5 == 0
+    'Buzz'
+  else
+    sum.to_s
   end
-  if sum % 5 == 0
-    answer += 'Buzz'
-  end
-  answer
 end
 
+puts fizz_buzz(1)
+puts fizz_buzz(2)
 puts fizz_buzz(3)
 puts fizz_buzz(4)
 puts fizz_buzz(5)
+puts fizz_buzz(11)
 puts fizz_buzz(12)
+puts fizz_buzz(13)
+puts fizz_buzz(14)
 puts fizz_buzz(15)
-puts fizz_buzz(18)
+puts fizz_buzz(16)
