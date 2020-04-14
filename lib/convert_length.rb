8fb564fbfc9abfc1c356baab228a72981f53a40a
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-14 20:17:16 +0900

    p.156 長さの単位変換プログラムを作成する

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,4 @@
+MAGNIFICATION = {m: 1, in: 39.37, ft: 3.28}
 def convert_length(length, unit_from, unit_to)
-  magnification = {'m': 1, 'in': 39.37, 'ft': 3.28}
-  (length * magnification[unit_to] / magnification[unit_from]).round(2)
+  (length * MAGNIFICATION[unit_to] / MAGNIFICATION[unit_from]).round(2)
 end
