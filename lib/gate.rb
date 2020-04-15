1fe8ddeb4356f8b4d71289f585430e7ce6886cb9
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-15 14:53:08 +0900

    p.221 改札機プログラムを自力で作成

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,2 +1,19 @@
 class Gate
+  STATIONS = [:umeda, :juso, :mikuni]
+  FARES = [150, 190]
+  def initialize(name)
+    @name = name
+  end
+
+  def enter(ticket)
+    ticket.stamp(@name)
+  end
+
+  def exit(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = (to - from).abs
+    @fare = FARES[distance - 1]
+    ticket.fare >= @fare
+  end
 end
