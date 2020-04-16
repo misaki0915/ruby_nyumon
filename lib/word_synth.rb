0cbe9ebea3bea668334db17a47fac9eed0a9ffa8
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-16 13:39:28 +0900

    p.378 ワードシンセサイザーを作成

--- lib/word_synth.rb
+++ lib/word_synth.rb
@@ -1,5 +1,15 @@
-module WordSynth
+class WordSynth
+  def initialize
+    @effects = []
+  end
+
+  def add_effect(effect)
+    @effects << effect
+  end
+
   def play(origin_words)
-    #
+    @effects.inject(origin_words) do |words, effect|
+      effect.call(words)
+    end
   end
 end
