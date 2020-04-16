25807e18e2779b4ac94071e7fe6c54b360e0a440
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-16 11:37:13 +0900

    p.358 メソッドの中身全体が例外処理で囲まれている

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,19 @@
 def fizz_buzz(sum)
-  if sum > 100
-    'Ruby'
-  elsif sum % 15 == 0
-    'Fizz Buzz'
-  elsif sum % 3 == 0
-    'Fizz'
-  elsif sum % 5 == 0
-    'Buzz'
-  else
-    sum.to_s
+  begin
+    if sum > 100
+      'Ruby'
+    elsif sum % 15 == 0
+      'Fizz Buzz'
+    elsif sum % 3 == 0
+      'Fizz'
+    elsif sum % 5 == 0
+      'Buzz'
+    else
+      sum.to_s
+    end
+  rescue => e
+    puts "#{e.class} #{e.message}"
   end
 end
+
+fizz_buzz(nil)
