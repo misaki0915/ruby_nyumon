d9a4d650d2dfd1d972d12c1cf75d2d018b430a55
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-16 12:43:23 +0900

    p.378 ワードシンセサイザーを作成するためのテスト

--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -3,8 +3,14 @@ require './lib/word_synth.rb'
 require './lib/effects.rb'
 
 class WordSynthTest < Minitest::Test
-  def test_play
-    assert WordSynth
-    assert Effects
+  def test_play_without_effects
+    synth = WordSynth.new
+    assert_equal 'Ruby is fun!', synth.play('Ruby is fun!')
+  end
+
+  def test_play_with_reverse
+    synth = WordSynth.new
+    synth.add_effect(Effects.reverse)
+    assert_equal 'ybuR si !nuf', synth.play('Ruby is fun!')
   end
 end
