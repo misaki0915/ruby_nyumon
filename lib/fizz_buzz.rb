aaf865131f9ee2d050e1c922e97a4e080ad6d7e9
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-16 11:39:53 +0900

    p.359 begin/endを省略した書き方

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,19 +1,17 @@
 def fizz_buzz(sum)
-  begin
-    if sum > 100
-      'Ruby'
-    elsif sum % 15 == 0
-      'Fizz Buzz'
-    elsif sum % 3 == 0
-      'Fizz'
-    elsif sum % 5 == 0
-      'Buzz'
-    else
-      sum.to_s
-    end
-  rescue => e
-    puts "#{e.class} #{e.message}"
+  if sum > 100
+    'Ruby'
+  elsif sum % 15 == 0
+    'Fizz Buzz'
+  elsif sum % 3 == 0
+    'Fizz'
+  elsif sum % 5 == 0
+    'Buzz'
+  else
+    sum.to_s
   end
+rescue => e
+  puts "#{e.class} #{e.message}"
 end
 
 fizz_buzz(nil)
