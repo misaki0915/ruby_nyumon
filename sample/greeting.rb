68b149c58cabbae7d65064362ecfa539447952a9
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-16 12:04:18 +0900

    p.369 ブロックに引数を渡す

--- sample/greeting.rb
+++ sample/greeting.rb
@@ -1,11 +1,21 @@
 def greeting
   puts 'おはよう'
-  if block_given?
-    yield
-  end
+  text = yield 'こんにちは', 12345
+  puts text
   puts 'こんばんは'
 end
 
-greeting do 
-  puts 'こんにちは'
+greeting do |text|
+  text * 2
+end
+
+def greeting
+  puts 'Hello'
+  text = yield 'Hi'
+  puts text
+  puts 'Good Night'
+end
+
+greeting do |text, other|
+  text * 2 + other.inspect
 end
