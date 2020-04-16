cca5b8d387590a2fe57887b335784968d396d8df
misaki0915 <cookie0915n@yahoo.co.jp>
2020-04-16 12:24:31 +0900

    p.375 Procオブジェクトを普通の引数のようにして渡す

--- sample/greeting.rb
+++ sample/greeting.rb
@@ -1,14 +1,11 @@
-def greeting(&block)
-  puts 'おはよう'
-  unless block.nil?
-    text = block.call('こんにちは')
-    puts text
-  end
-  puts 'こんばんは'
+def greeting(proc_1, proc_2, proc_3)
+  puts proc_1.call('おはよう')
+  puts proc_2.call('こんにちは')
+  puts proc_3.call('こんばんは')
 end
 
-greeting
+shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
+repeat_proc = Proc.new { |text| text * 2 }
+question_proc = Proc.new { |text| "#{text}?" }
 
-greeting do |text|
-  text * 2
-end
+greeting(shuffle_proc, repeat_proc, question_proc)
